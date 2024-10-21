16dee6c03a72e5b3854a8fb79531c98316141665
leonardojgm <cookie.maia@gmail.com>
2024-10-21 10:09:48 -0300

    Aulas-C#-Avançado

---
+++ Aula_03_LoopParallel/Aula_03_LoopParallel/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula_03_LoopParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                if(i == 99)
+                {
+                    System.Console.WriteLine($"O loop foi parado na posição {i}");
+                    break;
+                }
+            }
+
+            Parallel.For(0, 300, (int i, ParallelLoopState state)=> 
+            {
+                if (i == 99)
+                {
+                    System.Console.WriteLine($"O loop foi parado na posição {i}");
+                    state.Stop();
+                }
+            });
+
+            var lista = Enumerable.Range(0, 300);
+            Parallel.ForEach(lista, (int i, ParallelLoopState state) =>
+            {
+                if (i == 99)
+                {
+                    System.Console.WriteLine($"O loop foi parado na posição {i}");
+                    state.Break();
+                }
+            });
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Aula_05_Usando_AsParallel/Aula_05_Usando_AsParallel/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aula_05_Usando_AsParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = Enumerable.Range(0, 10);
+
+            var resultadoLista = 
+                from n in lista.AsParallel()
+                where BuscarNumeroPar(n)
+                select n;
+
+            Parallel.ForEach(resultadoLista, (i)=> 
+            {
+                System.Console.WriteLine(i);
+            });
+
+            System.Console.ReadKey();
+        }
+
+        private static bool BuscarNumeroPar(int n)
+        {
+            Thread.Sleep(1000);
+            return n % 2 == 0;
+        }
+    }
+}
